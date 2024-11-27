483516435b1eccd00ca9066e68a808541cdffa74
Ignatius Malala <Malala@UCLL.local>
2024-11-27 23:55:01 +0300

    Initial commit

---
+++ src/Account.java
@@ -0,0 +1,14 @@
+public class Member {
+
+    private String name;
+    private String sex;
+    private int age;
+    private int id;
+    private String startDate;
+    private String status;
+    private String membershipType;
+    private double fitnessLevel;
+    private int totalAttendances;
+    private int totalSessions;
+
+}
